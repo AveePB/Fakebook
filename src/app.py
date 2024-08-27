c5620a61b2166128b247daf73fa048e90ceb5c1b
AveePB <mike303.py@gmail.com>
2024-08-27 19:55:13 +0200

    Updated app

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from fakebook.authz.routes import authz_bp
 from fakebook.authz.tokens import validate_cookies
 from fakebook.database import mysql
 from fakebook.config import Config
+from fakebook.profile.routes import profile_bp
 from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
@@ -16,12 +17,15 @@ if (__name__ == '__main__'):
     # Update constants 
     app.config.from_object(Config)
 
-    #Pin database & blueprints
+    # Pin database & blueprints
     mysql.init_app(app)
+
     app.register_blueprint(authz_bp)
+    app.register_blueprint(profile_bp)
     
     # Create tables
     with app.app_context():
         mysql.create_all()
     
+    # Start application
     app.run()
