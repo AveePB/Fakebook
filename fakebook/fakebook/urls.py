612fa4c20c9a65bb182ec596764a79291a8d901a
AveePB <mike303.py@gmail.com>
2024-09-05 10:57:04 +0200

    Configured the media dir

--- fakebook/fakebook/urls.py
+++ fakebook/fakebook/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('auth/', include('authz.urls')),
     path('users/', include('users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
