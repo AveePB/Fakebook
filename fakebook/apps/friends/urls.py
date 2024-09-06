fb6b15f12b4be797ea2b989ba0c99c3adf6e4e30
AveePB <mike303.py@gmail.com>
2024-09-06 14:54:05 +0200

    Endpoints passed postman tests

--- fakebook/apps/friends/urls.py
+++ fakebook/apps/friends/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.friends.views import FriendsView
+from apps.friends.views import *
 
 urlpatterns = [
     path('', FriendsView.as_view(), name='friends'),
+    path('<int:user_id>/', FriendsView2.as_view(), name='friends2'),
 ]
